874c66f3e68b21d5175f7dfda2a310874ee7dce4
Ruan Melo <ruantmelo@gmail.com>
2021-10-23 17:38:13 -0300

    Finalizando aula

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,21 @@
+import { useContext, useEffect } from 'react';
+import {VscGithubInverted} from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth';
+import { api } from '../../services/api';
+import styles from './styles.module.scss';
+
+
+export const LoginBox = () => {
+  const {signInUrl} = useContext(AuthContext);
+
+  return(
+    <div className={styles.loginBoxWrapper}>
+
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href = {signInUrl} className = {styles.signInWithGitHub}>
+        <VscGithubInverted size= "24" />
+        Entrar com o GitHub
+      </a>
+    </div>
+  )
+}
